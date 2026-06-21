d373e2fa3b45a9b0f8ab038bd5c8139c5d58c067
danielhernandez123123 <daniel.hernandez.1@ug.uchile.cl>
2026-06-21 02:37:03 -0400

    Update actualizar_precios.py

--- scripts/actualizar_precios.py
+++ scripts/actualizar_precios.py
@@ -1,10 +1,35 @@
 import os
 import json
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
 cred_json = json.loads(os.environ["FIREBASE_CREDENTIALS"])
 
-print("PROJECT:")
-print(cred_json["project_id"])
+cred = credentials.Certificate(cred_json)
+firebase_admin.initialize_app(cred)
 
-print("\nCLIENT EMAIL:")
-print(cred_json["client_email"])
+db = firestore.client()
+
+uid = "xp6AChrnInM8eMy5NCquUFX1TOO2"
+
+doc = db.collection("users").document(uid).get()
+
+print("Existe:", doc.exists)
+
+if doc.exists:
+    print(doc.to_dict())
+
+    products = list(
+        db.collection("users")
+        .document(uid)
+        .collection("products")
+        .stream()
+    )
+
+    print("Productos:", len(products))
+
+    for p in products[:5]:
+        print(p.id)
+        print(p.to_dict())
