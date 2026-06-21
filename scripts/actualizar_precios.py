0c7e4c8a60a8470e504ba7f3bdfd022d0e5d2de5
danielhernandez123123 <daniel.hernandez.1@ug.uchile.cl>
2026-06-21 02:53:02 -0400

    Update actualizar_precios.py

--- scripts/actualizar_precios.py
+++ scripts/actualizar_precios.py
@@ -11,33 +11,18 @@ headers = {
     )
 }
 
-r = requests.get(url, headers=headers, timeout=30)
+html = requests.get(
+    url,
+    headers=headers,
+    timeout=30
+).text
 
-print("STATUS:", r.status_code)
+m = re.search(
+    r'"currentPrice":\{"price":(\d+)',
+    html
+)
 
-html = r.text
-
-patrones = [
-    "price",
-    "Price",
-    "currentPrice",
-    "salePrice",
-    "offerPrice",
-    "product-price",
-    "00780223007002",
-    "Trencito"
-]
-
-for patron in patrones:
-    print("\n===================")
-    print("BUSCANDO:", patron)
-
-    idx = html.find(patron)
-
-    if idx != -1:
-        inicio = max(0, idx - 200)
-        fin = min(len(html), idx + 500)
-
-        print(html[inicio:fin])
-    else:
-        print("NO ENCONTRADO")
+if m:
+    print("PRECIO ENCONTRADO:", int(m.group(1)))
+else:
+    print("NO ENCONTRADO")
